25fd6912581fc84977688a3b0539c821d99b394c
oalieno <jeffrey6910@gmail.com>
2019-03-21 12:25:34 -0500

    update

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -14,4 +17,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 
 # copy all file
-cp -r file/ ~/
+cp file/.* ~/
+
+# pyenv
+curl https://pyenv.run | bash
+pyenv install 3.7.0
+pyenv global 3.7.0
+
+# fuck
+pip install thefuck
